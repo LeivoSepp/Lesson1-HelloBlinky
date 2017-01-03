d1e507adefa21ae45841bf244ee7dcbb268b8cd8
leivos <leivos@TLNYOGAX1>
2017-01-03 22:22:56 +0200

    Add project files.

---
+++ HelloBlinky/StartupTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
+using System.Threading.Tasks;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace HelloBlinky
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            int LED_PIN = 35;
+            var gpio = GpioController.GetDefault();
+
+            GpioPin pin = gpio.OpenPin(LED_PIN);
+            pin.Write(GpioPinValue.High);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+
+            while(true)
+            {
+                pin.Write(GpioPinValue.Low);
+                Task.Delay(1000).Wait();
+                pin.Write(GpioPinValue.High);
+                Task.Delay(1000).Wait();
+            }
+            // 
+            // TODO: Insert code to perform background work
+            //
+            // If you start any asynchronous methods here, prevent the task
+            // from closing prematurely by using BackgroundTaskDeferral as
+            // described in http://aka.ms/backgroundtaskdeferral
+            //
+        }
+    }
+}
