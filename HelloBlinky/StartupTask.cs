328c21c7f24135027b6af78fa2df2f87e2193c7c
Leivo Sepp <leivo@robootika.com>
2017-01-03 22:30:04 +0200

    removeing unneccessary using

--- HelloBlinky/StartupTask.cs
+++ HelloBlinky/StartupTask.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
-using Windows.ApplicationModel.Background;
+﻿using Windows.ApplicationModel.Background;
 using Windows.Devices.Gpio;
 using System.Threading.Tasks;
 
@@ -29,13 +24,6 @@ namespace HelloBlinky
                 pin.Write(GpioPinValue.High);
                 Task.Delay(1000).Wait();
             }
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
         }
     }
 }
